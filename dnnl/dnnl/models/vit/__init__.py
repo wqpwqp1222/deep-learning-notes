f7a5675573b798eb5315d116d79f16a00657abdd
jshn9515 <jshn9510@gmail.com>
2026-05-14 00:45:12 -0400

    Import ViTPositionalEmbedding in the ViT model module

--- dnnl/dnnl/models/vit/__init__.py
+++ dnnl/dnnl/models/vit/__init__.py
@@ -1,3 +1,5 @@
+from .embedding import ViTEmbedding as ViTEmbedding
 from .embedding import ViTConvPatchEmbedding as ViTConvPatchEmbedding
 from .embedding import ViTLinearPatchEmbedding as ViTLinearPatchEmbedding
+from .embedding import ViTPositionalEmbedding as ViTPositionalEmbedding
 from .utils import patchify as patchify
