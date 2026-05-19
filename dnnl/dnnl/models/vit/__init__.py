53592514e4c95882417efe8502df6740643a4bd9
jshn9515 <jshn9510@gmail.com>
2026-05-19 17:41:36 -0400

    Reorder import statements in ViT module for improved organization

--- dnnl/dnnl/models/vit/__init__.py
+++ dnnl/dnnl/models/vit/__init__.py
@@ -1,5 +1,10 @@
-from .embedding import ViTEmbedding as ViTEmbedding
 from .embedding import ViTConvPatchEmbedding as ViTConvPatchEmbedding
+from .embedding import ViTEmbedding as ViTEmbedding
 from .embedding import ViTLinearPatchEmbedding as ViTLinearPatchEmbedding
 from .embedding import ViTPositionalEmbedding as ViTPositionalEmbedding
 from .utils import patchify as patchify
+from .vit import VisionTransformer as VisionTransformer
+from .vit import ViTClassificationHead as ViTClassificationHead
+from .vit import ViTEncoder as ViTEncoder
+from .vit import ViTEncoderLayer as ViTEncoderLayer
+from .vit import ViTMLP as ViTMLP
