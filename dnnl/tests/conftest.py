915c16a913a6cf838cfd553cd36afb300db4b288
jshn9515 <jshn9510@gmail.com>
2026-06-01 11:02:42 -0400

    Update PACKAGE_ROOT path in conftest.py to include 'src' directory

--- dnnl/tests/conftest.py
+++ dnnl/tests/conftest.py
@@ -1,5 +1,5 @@
 import sys
 from pathlib import Path
 
-PACKAGE_ROOT = Path(__file__).resolve().parents[1]
+PACKAGE_ROOT = Path(__file__).resolve().parents[1] / 'src'
 sys.path.insert(0, str(PACKAGE_ROOT))
