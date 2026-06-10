d38b0dd0ef11b85eeb0dc88cd2d9b12f6d7d94b6
jshn9515 <jshn9510@gmail.com>
2026-06-10 17:09:42 -0400

    Refactor optimizer default value tests to use signature extraction for improved clarity

--- dnnl/tests/optim/test_defaults.py
+++ dnnl/tests/optim/test_defaults.py
@@ -1,116 +1,47 @@
 import inspect
-from collections.abc import Callable
+from typing import Any
 
 import torch.optim as optim
 
 import dnnl.optim as dopt
 
 
-def _default(function: Callable, name: str):
-    return inspect.signature(function).parameters[name].default
+def _sig(function: Any, name: str):
+    sig = inspect.signature(function.__init__)
+    return sig.parameters[name].default
 
 
-def test_optimizer_defaults_match_torch_optim_defaults():
-    assert _default(dopt.SGD.__init__, 'lr') == _default(optim.SGD.__init__, 'lr')
-    assert _default(dopt.SGD.__init__, 'momentum') == _default(
-        optim.SGD.__init__,
-        'momentum',
-    )
-    assert _default(dopt.SGD.__init__, 'weight_decay') == _default(
-        optim.SGD.__init__,
-        'weight_decay',
-    )
-    assert _default(dopt.SGD.__init__, 'nesterov') == _default(
-        optim.SGD.__init__,
-        'nesterov',
+def test_optimizer_sigs_match_torch_optim_sigs():
+    assert _sig(dopt.SGD, 'lr') == _sig(optim.SGD, 'lr')
+    assert _sig(dopt.SGD, 'momentum') == _sig(optim.SGD, 'momentum')
+    assert _sig(dopt.SGD, 'weight_decay') == _sig(optim.SGD, 'weight_decay')
+    assert _sig(dopt.SGD, 'nesterov') == _sig(optim.SGD, 'nesterov')
+
+    assert _sig(dopt.Adagrad, 'lr') == _sig(optim.Adagrad, 'lr')
+    assert _sig(dopt.Adagrad, 'lr_decay') == _sig(optim.Adagrad, 'lr_decay')
+    assert _sig(dopt.Adagrad, 'eps') == _sig(optim.Adagrad, 'eps')
+    assert _sig(dopt.Adagrad, 'weight_decay') == _sig(optim.Adagrad, 'weight_decay')
+    assert _sig(dopt.Adagrad, 'initial_accumulator_value') == _sig(
+        optim.Adagrad, 'initial_accumulator_value'
     )
 
-    assert _default(dopt.Adagrad.__init__, 'lr') == _default(
-        optim.Adagrad.__init__,
-        'lr',
-    )
-    assert _default(dopt.Adagrad.__init__, 'lr_decay') == _default(
-        optim.Adagrad.__init__,
-        'lr_decay',
-    )
-    assert _default(dopt.Adagrad.__init__, 'eps') == _default(
-        optim.Adagrad.__init__,
-        'eps',
-    )
-    assert _default(dopt.Adagrad.__init__, 'weight_decay') == _default(
-        optim.Adagrad.__init__,
-        'weight_decay',
-    )
-    assert _default(dopt.Adagrad.__init__, 'initial_accumulator_value') == _default(
-        optim.Adagrad.__init__,
-        'initial_accumulator_value',
-    )
+    assert _sig(dopt.RMSprop, 'lr') == _sig(optim.RMSprop, 'lr')
+    assert _sig(dopt.RMSprop, 'rho') == _sig(optim.RMSprop, 'alpha')
+    assert _sig(dopt.RMSprop, 'eps') == _sig(optim.RMSprop, 'eps')
+    assert _sig(dopt.RMSprop, 'weight_decay') == _sig(optim.RMSprop, 'weight_decay')
+    assert _sig(dopt.RMSprop, 'momentum') == _sig(optim.RMSprop, 'momentum')
 
-    assert _default(dopt.RMSprop.__init__, 'lr') == _default(
-        optim.RMSprop.__init__,
-        'lr',
-    )
-    assert _default(dopt.RMSprop.__init__, 'rho') == _default(
-        optim.RMSprop.__init__,
-        'alpha',
-    )
-    assert _default(dopt.RMSprop.__init__, 'eps') == _default(
-        optim.RMSprop.__init__,
-        'eps',
-    )
-    assert _default(dopt.RMSprop.__init__, 'weight_decay') == _default(
-        optim.RMSprop.__init__,
-        'weight_decay',
-    )
-    assert _default(dopt.RMSprop.__init__, 'momentum') == _default(
-        optim.RMSprop.__init__,
-        'momentum',
-    )
+    assert _sig(dopt.Adadelta, 'lr') == _sig(optim.Adadelta, 'lr')
+    assert _sig(dopt.Adadelta, 'rho') == _sig(optim.Adadelta, 'rho')
+    assert _sig(dopt.Adadelta, 'eps') == _sig(optim.Adadelta, 'eps')
+    assert _sig(dopt.Adadelta, 'weight_decay') == _sig(optim.Adadelta, 'weight_decay')
 
-    assert _default(dopt.Adadelta.__init__, 'lr') == _default(
-        optim.Adadelta.__init__,
-        'lr',
-    )
-    assert _default(dopt.Adadelta.__init__, 'rho') == _default(
-        optim.Adadelta.__init__,
-        'rho',
-    )
-    assert _default(dopt.Adadelta.__init__, 'eps') == _default(
-        optim.Adadelta.__init__,
-        'eps',
-    )
-    assert _default(dopt.Adadelta.__init__, 'weight_decay') == _default(
-        optim.Adadelta.__init__,
-        'weight_decay',
-    )
+    assert _sig(dopt.Adam, 'lr') == _sig(optim.Adam, 'lr')
+    assert _sig(dopt.Adam, 'betas') == _sig(optim.Adam, 'betas')
+    assert _sig(dopt.Adam, 'eps') == _sig(optim.Adam, 'eps')
+    assert _sig(dopt.Adam, 'weight_decay') == _sig(optim.Adam, 'weight_decay')
 
-    assert _default(dopt.Adam.__init__, 'lr') == _default(optim.Adam.__init__, 'lr')
-    assert _default(dopt.Adam.__init__, 'betas') == _default(
-        optim.Adam.__init__,
-        'betas',
-    )
-    assert _default(dopt.Adam.__init__, 'eps') == _default(
-        optim.Adam.__init__,
-        'eps',
-    )
-    assert _default(dopt.Adam.__init__, 'weight_decay') == _default(
-        optim.Adam.__init__,
-        'weight_decay',
-    )
-
-    assert _default(dopt.AdamW.__init__, 'lr') == _default(
-        optim.AdamW.__init__,
-        'lr',
-    )
-    assert _default(dopt.AdamW.__init__, 'betas') == _default(
-        optim.AdamW.__init__,
-        'betas',
-    )
-    assert _default(dopt.AdamW.__init__, 'eps') == _default(
-        optim.AdamW.__init__,
-        'eps',
-    )
-    assert _default(dopt.AdamW.__init__, 'weight_decay') == _default(
-        optim.AdamW.__init__,
-        'weight_decay',
-    )
+    assert _sig(dopt.AdamW, 'lr') == _sig(optim.AdamW, 'lr')
+    assert _sig(dopt.AdamW, 'betas') == _sig(optim.AdamW, 'betas')
+    assert _sig(dopt.AdamW, 'eps') == _sig(optim.AdamW, 'eps')
+    assert _sig(dopt.AdamW, 'weight_decay') == _sig(optim.AdamW, 'weight_decay')
