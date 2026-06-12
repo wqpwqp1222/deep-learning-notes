cbc9cba8b43c0f82c0f508961e7d0681be3c0555
jshn9515 <jshn9510@gmail.com>
2026-06-11 21:40:55 -0400

    Add Identity and Linear modules for affine transformations

---
+++ dnnl/src/dnnl/nn/linear.py
@@ -0,0 +1,73 @@
+import math
+
+import torch
+import torch.nn as nn
+from torch import Tensor
+
+from . import functional as dF
+
+__all__ = [
+    'Identity',
+    'Linear',
+]
+
+
+class Identity(nn.Module):
+    """A module that returns the input as is."""
+
+    def __init__(self):
+        """Initialize the Identity module."""
+        super().__init__()
+
+    def forward(self, x: Tensor) -> Tensor:
+        """Return the input as is."""
+        return x
+
+
+class Linear(nn.Module):
+    """Apply an affine transformation to the incoming data."""
+
+    def __init__(
+        self,
+        in_features: int,
+        out_features: int,
+        bias: bool = True,
+    ):
+        """Initialize the weight and optional bias parameters.
+
+        Args:
+            in_features (int): Size of each input sample.
+            out_features (int): Size of each output sample.
+            bias (bool, default: True): Whether to learn an additive bias.
+        """
+        super().__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+
+        weight = torch.empty(out_features, in_features)
+        self.weight = nn.Parameter(weight)
+
+        if bias:
+            self.bias = nn.Parameter(torch.empty(out_features))
+        else:
+            self.register_parameter('bias', None)
+
+        self.reset_parameters()
+
+    def reset_parameters(self) -> None:
+        """Reset parameters using the same default initialization as PyTorch."""
+        nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        # A simplified version
+        if self.bias is not None:
+            nn.init.uniform_(self.bias)
+
+    def forward(self, x: Tensor) -> Tensor:
+        """Apply the linear transformation."""
+        return dF.linear(x, self.weight, self.bias)
+
+    def extra_repr(self) -> str:
+        return (
+            f'in_features={self.in_features}, '
+            f'out_features={self.out_features}, '
+            f'bias={self.bias is not None}'
+        )
