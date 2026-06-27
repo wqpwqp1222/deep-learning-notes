0a22be6727fe692c01595a5fbc5a37100583b3b1
jshn9515 <jshn9510@gmail.com>
2026-06-27 16:55:47 -0400

    Add activation and normalization functions to __init__.py

--- dnnlpy/src/dnnlpy/nn/functional/__init__.py
+++ dnnlpy/src/dnnlpy/nn/functional/__init__.py
@@ -1,18 +1,54 @@
-from .activation import gelu as gelu
-from .activation import log_softmax as log_softmax
-from .activation import relu as relu
-from .activation import sigmoid as sigmoid
-from .activation import softmax as softmax
-from .activation import tanh as tanh
+from .activation import (
+    celu as celu,
+    elu as elu,
+    gelu as gelu,
+    hardsigmoid as hardsigmoid,
+    hardshrink as hardshrink,
+    hardswish as hardswish,
+    hardtanh as hardtanh,
+    leaky_relu as leaky_relu,
+    log_softmax as log_softmax,
+    logsigmoid as logsigmoid,
+    mish as mish,
+    prelu as prelu,
+    relu as relu,
+    relu6 as relu6,
+    rrelu as rrelu,
+    selu as selu,
+    sigmoid as sigmoid,
+    silu as silu,
+    softmin as softmin,
+    softmax as softmax,
+    softplus as softplus,
+    softshrink as softshrink,
+    softsign as softsign,
+    tanh as tanh,
+    tanhshrink as tanhshrink,
+    threshold as threshold,
+)
 from .affine import linear as linear
-from .attention import generate_causal_mask as generate_causal_mask
-from .attention import multi_head_attention as multi_head_attention
-from .attention import naive_attention as naive_attention
-from .attention import scaled_dot_product_attention as scaled_dot_product_attention
-from .flash_attention import flash_attention_v1_backward as flash_attention_v1_backward
-from .flash_attention import flash_attention_v1_forward as flash_attention_v1_forward
+from .attention import (
+    generate_causal_mask as generate_causal_mask,
+    multi_head_attention as multi_head_attention,
+    naive_attention as naive_attention,
+    scaled_dot_product_attention as scaled_dot_product_attention,
+)
+from .flash_attention import (
+    flash_attention_v1_backward as flash_attention_v1_backward,
+    flash_attention_v1_forward as flash_attention_v1_forward,
+)
 from .loss import cross_entropy as cross_entropy
-from .regularization import dropout as dropout
-from .regularization import dropout1d as dropout1d
-from .regularization import dropout2d as dropout2d
-from .regularization import dropout3d as dropout3d
+from .normalization import (
+    batch_norm as batch_norm,
+    group_norm as group_norm,
+    instance_norm as instance_norm,
+    layer_norm as layer_norm,
+    local_response_norm as local_response_norm,
+    rms_norm as rms_norm,
+)
+from .regularization import (
+    dropout as dropout,
+    dropout1d as dropout1d,
+    dropout2d as dropout2d,
+    dropout3d as dropout3d,
+)
