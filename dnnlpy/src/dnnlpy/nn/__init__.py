8cb3ba4e251ae9dc1de882c91cdab51db5620bcd
jshn9515 <jshn9510@gmail.com>
2026-06-27 17:00:15 -0400

    Add PyTorch activation and normalization module to __init__.py

--- dnnlpy/src/dnnlpy/nn/__init__.py
+++ dnnlpy/src/dnnlpy/nn/__init__.py
@@ -1,21 +1,58 @@
-from .activation import GELU as GELU
-from .activation import LogSoftmax as LogSoftmax
-from .activation import ReLU as ReLU
-from .activation import Sigmoid as Sigmoid
-from .activation import Softmax as Softmax
-from .activation import Tanh as Tanh
-from .affine import Identity as Identity
-from .affine import Linear as Linear
+from .activation import (
+    CELU as CELU,
+    ELU as ELU,
+    GELU as GELU,
+    SELU as SELU,
+    HardShrink as HardShrink,
+    HardSigmoid as HardSigmoid,
+    HardSwish as HardSwish,
+    HardTanh as HardTanh,
+    LeakyReLU as LeakyReLU,
+    LogSigmoid as LogSigmoid,
+    LogSoftmax as LogSoftmax,
+    Mish as Mish,
+    PReLU as PReLU,
+    ReLU as ReLU,
+    ReLU6 as ReLU6,
+    RReLU as RReLU,
+    Sigmoid as Sigmoid,
+    SiLU as SiLU,
+    Softmax as Softmax,
+    Softmin as Softmin,
+    Softplus as Softplus,
+    SoftShrink as SoftShrink,
+    SoftSign as SoftSign,
+    Tanh as Tanh,
+    TanhShrink as TanhShrink,
+    Threshold as Threshold,
+)
+from .affine import Identity as Identity, Linear as Linear
 from .attention import MultiheadAttention as MultiheadAttention
 from .loss import CrossEntropyLoss as CrossEntropyLoss
-from .regularization import Dropout as Dropout
-from .regularization import Dropout1d as Dropout1d
-from .regularization import Dropout2d as Dropout2d
-from .regularization import Dropout3d as Dropout3d
-from .transformer import LearnablePositionalEmbedding as LearnablePositionalEmbedding
-from .transformer import SinusoidalPositionalEncoding as SinusoidalPositionalEncoding
-from .transformer import Transformer as Transformer
-from .transformer import TransformerDecoder as TransformerDecoder
-from .transformer import TransformerDecoderLayer as TransformerDecoderLayer
-from .transformer import TransformerEncoder as TransformerEncoder
-from .transformer import TransformerEncoderLayer as TransformerEncoderLayer
+from .normalization import (
+    BatchNorm1d as BatchNorm1d,
+    BatchNorm2d as BatchNorm2d,
+    BatchNorm3d as BatchNorm3d,
+    GroupNorm as GroupNorm,
+    InstanceNorm1d as InstanceNorm1d,
+    InstanceNorm2d as InstanceNorm2d,
+    InstanceNorm3d as InstanceNorm3d,
+    LayerNorm as LayerNorm,
+    LocalResponseNorm as LocalResponseNorm,
+    RMSNorm as RMSNorm,
+)
+from .regularization import (
+    Dropout as Dropout,
+    Dropout1d as Dropout1d,
+    Dropout2d as Dropout2d,
+    Dropout3d as Dropout3d,
+)
+from .transformer import (
+    LearnablePositionalEmbedding as LearnablePositionalEmbedding,
+    SinusoidalPositionalEncoding as SinusoidalPositionalEncoding,
+    Transformer as Transformer,
+    TransformerDecoder as TransformerDecoder,
+    TransformerDecoderLayer as TransformerDecoderLayer,
+    TransformerEncoder as TransformerEncoder,
+    TransformerEncoderLayer as TransformerEncoderLayer,
+)
