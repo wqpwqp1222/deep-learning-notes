56f6c4c986d343d939bb87deed74692e7b202102
jshn9515 <jshn9510@gmail.com>
2026-06-28 19:29:26 -0400

    Add bilinear transformation function implementation

--- dnnlpy/src/dnnlpy/nn/functional/affine.py
+++ dnnlpy/src/dnnlpy/nn/functional/affine.py
@@ -1,6 +1,10 @@
+import torch
 from torch import Tensor
 
-__all__ = ['linear']
+__all__ = [
+    'bilinear',
+    'linear',
+]
 
 
 def linear(x: Tensor, weight: Tensor, bias: Tensor | None = None) -> Tensor:
@@ -15,8 +19,31 @@ def linear(x: Tensor, weight: Tensor, bias: Tensor | None = None) -> Tensor:
         Tensor: Transformed tensor with shape ``(*, out_features)``.
     """
     if x.ndim == 2 and bias is not None:
-        return bias.addmm(x, weight.T)
+        return torch.addmm(bias, x, weight.T)
     elif bias is not None:
         return x @ weight.T + bias
     else:
         return x @ weight.T
+
+
+def bilinear(
+    x1: Tensor,
+    x2: Tensor,
+    weight: Tensor,
+    bias: Tensor | None = None,
+) -> Tensor:
+    """Apply a bilinear transformation to two incoming tensors.
+
+    Args:
+        x1 (Tensor): First input tensor with shape ``(*, in1_features)``.
+        x2 (Tensor): Second input tensor with shape ``(*, in2_features)``.
+        weight (Tensor): Weight tensor with shape``(out_features, in1_features, in2_features)``.
+        bias (Tensor | None, default: None): Optional bias tensor with shape ``(out_features,)``.
+
+    Returns:
+        Tensor: Transformed tensor with shape ``(*, out_features)``.
+    """
+    y = torch.einsum('...i,oij,...j->...o', x1, weight, x2)
+    if bias is not None:
+        y = y + bias
+    return y
